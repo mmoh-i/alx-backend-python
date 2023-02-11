96f53e5500021871534ca5217f09245088893b74
mmoh-i <ibrahimmusty100@yahoo.com>
2023-02-11 13:20:18 -0800

    typed_annotation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def concat(str1: str, str2: str) -> str:
+    # concat typed annotation
+    # accept argument to be str and
+    # also returns a string.
+    return str1 + str2
