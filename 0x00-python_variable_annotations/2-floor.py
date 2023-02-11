0e61492872f2c294e6f04243d086ef471eb8581d
mmoh-i <ibrahimmusty100@yahoo.com>
2023-02-11 13:43:32 -0800

    Annotation

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import math #importing math function.
+
+
+def floor(n: float) -> float:
+    # floor: takes in a float argument, n: floor argument
+    # expected to return a  float.
+    return math.floor(n)
