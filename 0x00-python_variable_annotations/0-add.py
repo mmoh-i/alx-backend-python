15f7b3a88ab64f5e89cd58ee044de3762bfdaf3e
mmoh-i <ibrahimmusty100@yahoo.com>
2023-02-11 15:28:15 -0800

    add()

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# The code to test for annotaion for two
+# variables x & y
+
 def add(x: float, y: float) -> float:
     # Python Annotation of the add function
     # which will accept x, y as both float
