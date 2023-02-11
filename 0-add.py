17f950bcb7d493e63c5318d72e4f61cca23312a3
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-02-11 22:07:32 +0100

    Create 0-add.py

---
+++ 0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def add(x: float, y: float) -> float:
+    # python Annotation of the add function
+    # which will accept x, y as both float
+    # and also return a float.
+    return x + y
