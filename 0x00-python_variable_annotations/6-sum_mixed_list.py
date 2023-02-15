6ea366d4a3a1472b27e04ea6bfa60f628dca1430
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-02-15 19:41:55 +0100

    Task-6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def sum_mixed_list(mxd_lst: [int, float]) -> float:
+    '''function to accept a list with both
+       int and float dat types and
+       float sum of objects in mxd_lst
+    '''
+    return sum(mxd_lst)
