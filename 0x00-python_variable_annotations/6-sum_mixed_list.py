08bcb6f39cc17b138e6711cb51debc98679b3b36
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-02-15 19:45:10 +0100

    Task-6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-def sum_mixed_list(mxd_lst: [int, float]) -> float:
+from typing import Union
+
+
+def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
     '''function to accept a list with both
        int and float dat types and
        float sum of objects in mxd_lst
