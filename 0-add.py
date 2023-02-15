687472851f0538506f371762bc16a06769a6815e
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-02-15 19:32:00 +0100

    Delete 0-add.py

--- 0-add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-def add(x: float, y: float) -> float:
-    # python Annotation of the add function
-    # which will accept x, y as both float
-    # and also return a float.
-    return x + y
