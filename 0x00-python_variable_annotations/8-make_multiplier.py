c0775a040f738a90c43ffe44f5b0f6061856bd2c
mmoh-i <ibrahimmusty100@yahoo.com>
2023-02-17 02:40:11 -0800

    Task-8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''Callable to return functioin when the make_multiplier
+    function is called, it takes float multiplier as args
+    :multiplier: float
+    '''
+    return lambda x: x*multiplier
