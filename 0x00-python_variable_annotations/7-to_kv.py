722aea33c834f23f7f99dc8aeed9d64d6916fd2d
mmoh-i <ibrahimmusty100@yahoo.com>
2023-02-17 02:23:43 -0800

    Task-7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,4 +3,8 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''to_kv functioin to return tuple with variables
+    :k: type str and
+    :v: to accept type in or float
+    '''
     return (k, (v)**2)
