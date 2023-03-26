1172d728200865bbb55f44c3496b2361526d281c
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-03-26 09:39:57 +0100

    Measure the runtime

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -9,9 +9,9 @@ wait_n = __import__('1-concurrent_coroutines.py').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> Float:
-    """ asyncio to return oprt** time.
+    """ asyncio to return oprtion time.
     """
-    start_time = time.perf_counter()
+    start_time = time.time()
     await.run(wait_n(n, max_delay))
-    final_time = (start_time - time.perf_counter()) / n
+    final_time = (start_time - time.time()) / n
     return final_time
