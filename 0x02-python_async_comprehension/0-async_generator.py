db3a0092d8fb68ee2def2ad8d65964e9b8331fae
mmoh-i <ibrahimmusty100@yahoo.com>
2023-04-07 04:24:23 -0700

    0. Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """Coroutine that takes
     no arguments
     """
