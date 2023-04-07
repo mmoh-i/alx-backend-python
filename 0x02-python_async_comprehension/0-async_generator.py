e1bda008f7a9e7dc6bc7c548183e1a73cab18178
mmoh-i <ibrahimmusty100@yahoo.com>
2023-04-07 04:21:02 -0700

    0. Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,11 +6,11 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """Coroutine that takes
     no arguments
     """
     for i in range(10):
 
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
