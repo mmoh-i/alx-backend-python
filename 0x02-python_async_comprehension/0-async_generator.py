f380c6186a9cbd365fa0e263f2a7e37d6869417b
mmoh-i <ibrahimmusty100@yahoo.com>
2023-04-07 04:13:22 -0700

    0. Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -13,4 +13,4 @@ async def async_generator() -> AsyncGenerator[int, None]:
     for i in range(10):
 
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.randint(0, 10)
