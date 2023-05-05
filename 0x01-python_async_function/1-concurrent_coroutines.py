8f2c0acc79dea4596758f5a2ec5861c12171a85b
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-05-05 15:34:52 +0100

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python3
-"""Task 1
-"""
+"""Task 1 module"""
 import asyncio
 from typing import List
-
-
 wait_random = __import__('0-basic_async_syntax').wait_random
-
-
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Executes wait_random n times.
-    """
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+    """Executing multiple caroutines at the same time with async"""
+    tasks = (wait_random(max_delay) for i in range(n))
+    result = await asyncio.gather(*tasks)
+    return (sorted(result))
