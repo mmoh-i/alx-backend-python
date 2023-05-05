0985139d6c7ae1c754ae717c5a79857f0868bd45
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-05-05 23:04:51 +0100

    5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-from typing import List
-def sum_list(inputlist: List[float]) -> float:
-    """
+"""
     sumlist function takes in a flaot list
-    :inpitlist: float 
+    :inpitlist: float
     and also returns sum of the float list
     """
+from typing import List
+
+
+def sum_list(inputlist: List[float]) -> float:
     return sum(inputlist)
