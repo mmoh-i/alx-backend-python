351634a9cd54285ef9e93b5dcc1ac2794d61349d
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-05-05 23:28:41 +0100

    101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
+""" Task 11 module """
+from typing import TypeVar, Union, Mapping, Any
 
-def safely_get_value(dct, key, default = None):
+
+T = TypeVar("T")
+Def = Union[T, None]
+res = Union[Any, T]
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> res:
+    """Implementing the Type Var annotation"""
     if key in dct:
         return dct[key]
     else:
